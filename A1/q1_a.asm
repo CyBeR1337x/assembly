5b87d40a1c9bc960a6260a65f3126837f4c53c6f
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2023-03-12 17:03:42 +0500

    Create

---
+++ A1/q1_a.asm
@@ -0,0 +1,19 @@
+.MODEL SMALL
+.STACK 100H 
+.DATA
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    MOV AH, 1
+    INT 21H
+    
+    MOV AH, 2
+    MOV DL, AL
+    INT 21H
+    
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
